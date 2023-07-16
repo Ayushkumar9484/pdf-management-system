d639e62899c33d53885fd64a2a282eb721c23d06
Ayush <9484ayush@gmail.com>
2023-07-16 13:47:03 +0530

    updated login and signup page

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -3,6 +3,7 @@ import "./style.css"
 import { Link, useNavigate } from 'react-router-dom'
 import { CurrentUser } from '../componenets/Usercontext'
 import './style.css'
+import LoginImage from '../images/LoginPageImage.png'
 
 
 export default function Login() {
@@ -50,29 +51,36 @@ export default function Login() {
 
   return (
     <>
-      {error && <h1>{error}</h1>}
-      <div className=' signin-card'>
-        <div className='login template d-flex justify-content-center align-items-center signin-card-inner' >
-          <div className='form_container p-5 rounded bg-white main-card-signin'>
-            <form onSubmit={handlesubmit}>
-              <h3 className='text-center'>Sign In</h3>
-              <div className='mb-2'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' name='email' value={LoginDetails.email} placeholder='Enter Email' className='form-control' onChange={handlechanges} />
+      <div className='signin-card-outer 100vh'>
+        <div className=' signin-card'>
+          {error && <h1>{error}</h1>}
+          <div className='login template d-flex rounded-4 signin-card-inner' >
+            <div className='image-container-signin rounded-4'>
+              <img src={LoginImage} width='500px' height='500px' alt='Login' />
+            </div>
+            <div className='main-card-sighin-outer rounded-4'>
+              <div className='form_container p-5 rounded main-card-signin'>
+                <form onSubmit={handlesubmit}>
+                  <h3 className='text-center' style={{fontFamily: "Gill Sans Extrabold"}}>Sign In</h3>
+                  <div className='mb-2'>
+                    <label htmlFor='email' style={{fontFamily: "Gill Sans Extrabold"}}>Email</label>
+                    <input type='email' name='email' value={LoginDetails.email} placeholder='Enter Email' className='form-control' onChange={handlechanges} />
+                  </div>
+                  <div className='mb-2 mt-2'>
+                    <label htmlFor='password' style={{fontFamily: "Gill Sans Extrabold"}}>Password</label>
+                    <input type='password' name='password' value={LoginDetails.password} placeholder='Enter Password' className='form-control' onChange={handlechanges} />
+                  </div>
+                  {/* <div className='mb-2'>
+                    <input type='checkbox' className='custom-control custom-checkbox' id='check' />
+                    <label htmlFor='check' className='custom-input-label ms-2'> Remember Me</label>
+                  </div> */}
+                  <div className='d-grid'>
+                    <button type='submit' className='btn btn-primary mt-2'  style={{fontFamily: "Gill Sans Extrabold",backgroundColor:"#6a378a"}}>Sign In</button>
+                  </div>
+                  <p className='text-end mt-2'><Link to="/signup" style={{fontFamily: "Gill Sans Extrabold"}}>Signup</Link></p>
+                </form>
               </div>
-              <div className='mb-2'>
-                <label htmlFor='password'>Password</label>
-                <input type='password' name='password' value={LoginDetails.password} placeholder='Enter Password' className='form-control' onChange={handlechanges} />
-              </div>
-              <div className='mb-2'>
-                <input type='checkbox' className='custom-control custom-checkbox' id='check' />
-                <label htmlFor='check' className='custom-input-label ms-2'> Remember Me</label>
-              </div>
-              <div className='d-grid'>
-                <button type='submit' className='btn btn-primary'>Sign In</button>
-              </div>
-              <p className='text-end mt-2'><Link to="/signup">Signup</Link></p>
-            </form>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import './style.css'
+import SignUpImage from '../images/SignUpPageImage.jpg'
 
 
 export default function Signup() {
   const navigate = useNavigate()
+  const [error, setError] = useState("")
   const [SignUpDetails, SetSignUpDetails] = useState({
     name: "",
     email: "",
@@ -43,38 +45,48 @@ export default function Signup() {
       })
   }
   return (
-    <div className='signin-card'>
-      <div className='signup template d-flex justify-content-center align-items-center signup-card-inner' >
-        <div className='form_container p-5 rounded bg-white main-card-signup'>
-          <form onSubmit={handlesubmit}>
-            <h3 className='text-center'>Sign Up</h3>
-            <div className='mb-2 '>
-              <label htmlFor='name'>Name</label>
-              <input type='text' name='name' value={SignUpDetails.name} placeholder='Enter Name' className='form-control w-100' onChange={(e) => handlechanges(e)} required />
+    <>
+      <div className='signup-card-outer 100vh'>
+        <div className='signin-card'>
+          {error && <h1>{error}</h1>}
+          <div className='signup template d-flex rounded-4 signup-card-inner'>
+            <div className='main-card-sighup-outer rounded-4'>
+              <div className='form_container p-5 rounded bg-white main-card-signup'>
+                <form onSubmit={handlesubmit}>
+                  <h3 className='text-center' style={{fontFamily: "Gill Sans Extrabold"}}>Sign Up</h3>
+                  <div className='mb-2 '>
+                    <label htmlFor='name' style={{fontFamily: "Gill Sans Extrabold"}}>Name</label>
+                    <input type='text' name='name' value={SignUpDetails.name} placeholder='Enter Name' className='form-control w-100' onChange={(e) => handlechanges(e)} required />
+                  </div>
+                  <div className='mb-2'>
+                    <label htmlFor='email'style={{fontFamily: "Gill Sans Extrabold"}}>Email</label>
+                    <input type='email' name='email' value={SignUpDetails.email} placeholder='Enter Email' className='form-control' onChange={handlechanges} required />
+                  </div>
+                  <div className='mb-2'>
+                    <label htmlFor='password' style={{fontFamily: "Gill Sans Extrabold"}}>Password</label>
+                    <input type='password' name='password' value={SignUpDetails.password} placeholder='Enter Password' className='form-control' onChange={handlechanges} required />
+                  </div>
+                  <div className='mb-2'>
+                    <label htmlFor='password' style={{fontFamily: "Gill Sans Extrabold"}}>Confirm Password</label>
+                    <input type='password' name='reEnterPassword' value={SignUpDetails.reEnterPassword} placeholder='Enter Password' className='form-control' onChange={handlechanges} required />
+                  </div>
+                  {/* <div className='mb-2'>
+                    <input type='checkbox' className='custom-control custom-checkbox' id='check' />
+                    <label htmlFor='check' className='custom-input-label ms-2'> Remember Me</label>
+                  </div> */}
+                  <div className='d-grid'>
+                    <button type='submit' className='btn btn-primary' style={{fontFamily: "Gill Sans Extrabold"}}>Sign Up</button>
+                  </div>
+                  <p className='text-end mt-2' style={{fontFamily: "Gill Sans Extrabold"}}>Already Registered? <Link to="/login" >Sign In</Link></p>
+                </form>
+              </div>
             </div>
-            <div className='mb-2'>
-              <label htmlFor='email'>Email</label>
-              <input type='email' name='email' value={SignUpDetails.email} placeholder='Enter Email' className='form-control' onChange={handlechanges} required />
+            <div className='image-container-signup rounded-4'>
+              <img src={SignUpImage} width='500px' height='500px' alt='Signup' />
             </div>
-            <div className='mb-2'>
-              <label htmlFor='password'>Password</label>
-              <input type='password' name='password' value={SignUpDetails.password} placeholder='Enter Password' className='form-control' onChange={handlechanges} required />
-            </div>
-            <div className='mb-2'>
-              <label htmlFor='password'>Confirm Password</label>
-              <input type='password' name='reEnterPassword' value={SignUpDetails.reEnterPassword} placeholder='Enter Password' className='form-control' onChange={handlechanges} required />
-            </div>
-            <div className='mb-2'>
-              <input type='checkbox' className='custom-control custom-checkbox' id='check' />
-              <label htmlFor='check' className='custom-input-label ms-2'> Remember Me</label>
-            </div>
-            <div className='d-grid'>
-              <button type='submit' className='btn btn-primary'>Sign Up</button>
-            </div>
-            <p className='text-end mt-2'>Already Registered? <Link to="/login">Sign In</Link></p>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
